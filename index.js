033f2763916b0764f4cec3961af0e2baa6b880be
vespakoen <k.schmeets@gmail.com>
2019-04-18 17:30:43 +0200

    Fix copying

--- index.js
+++ index.js
@@ -92,7 +92,7 @@ exports.createConfig = function createConfig(opts) {
     plugins: opts.type === 'cep' ? [
       new WriteFilePlugin(),
       new CopyPlugin([
-        { from: 'public/', to: 'dist/' }
+        { from: 'public/', to: '.' }
       ]),
       new CepWebpackPlugin({
         devPort: opts.hasOwnProperty('devPort') ? opts.devPort : 8080,
